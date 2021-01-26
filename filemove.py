48d06370e0f9d74998ee5d933f7267d140dfdd9c
Astrov <vpopovic003@gmail.com>
2021-01-26 16:05:10 +0100

    script completition

--- filemove.py
+++ filemove.py
@@ -9,20 +9,23 @@ def numericalSort(value):
     parts[1::2] = map(int, parts[1::2])
     return parts
 
-for file in sorted(glob.glob("test\\*.mp4"), key=numericalSort):
-
-    if not file:
+print('moving file')
+for file_new in sorted(glob.glob("videos\\*.mp4"), key=numericalSort):
+    if not file_new:
         break
-    video_number = os.path.abspath(file)
 
-print(video_number)
-video_number = video_number.replace(file, "")
-print(video_number)
-# video_number = video_number[:10]
-# video_number = video_number[-3:]
-# count_number = int(video_number) + 1
-#
-# file_name = file.replace("videos\\", "")
-# file_name = file_name[3:]
-# #print('Video: ' + file + ' renamed to: ' + str(file_name))
-# os.rename(file, "test\\" + str(count_number) + file_name)
+    for last_file_published in sorted(glob.glob("D:\\396\\Miran Rubin Video\\published\\*.mp4"), key=numericalSort):
+        if not last_file_published:
+            break
+
+    video_number = last_file_published
+video_number = video_number.replace("D:\\396\\Miran Rubin Video\\published\\","")
+
+count_number = video_number[:3]
+count_number_new = int(count_number) + 1
+
+file_name = file_new.replace("videos\\", "")
+file_name = file_name.replace(count_number, str(count_number_new))
+file_name = file_name[3:]
+shutil.move(file_new, "D:\\396\\Miran Rubin Video\\published\\" + str(count_number_new) + file_name)
+print('Video: ' + file_new + ' moved to: "D:\\396\\Miran Rubin Video\\published\\' + str(count_number_new) + str(file_name))
