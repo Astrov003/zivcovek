ecd93283eefaba3f60ccd6a7ee6c3ef69150eab4
Astrov <vpopovic003@gmail.com>
2021-01-26 16:05:39 +0100

    some tweeks

--- zivcovek.py
+++ zivcovek.py
@@ -1,18 +1,21 @@
 import os
 
 urllist = open("resources/video_list.txt", "r")
-url = urllist.readline()
+uploading_url = urllist.readline()
+uploading_url = uploading_url.replace('\n', '')
 
-urllist = open("logs/uploaded.log", "r")
-uploaded = urllist.readline()
+urllist2 = open("logs/uploaded.log", "r")
+uploaded_url = urllist2.readline()
+uploaded_url = uploaded_url.replace('\n', '')
 
-if url != uploaded:
+
+if uploading_url != uploaded_url:
     exec(open('fbdownloader.py').read())
     exec(open('ytuploader.py').read())
 
     output_file = open('logs/uploaded.log','w', encoding="utf-8") #save last URL to log chech for double
-    print(url, file=output_file)
+    print(uploading_url, file=output_file)
 
     exec(open('filemove.py').read())
 else:
-    print(url + ' already posted on Youtube/Ziv Covek')
+    print("<" + uploading_url + ">" + ' already posted on Youtube/Ziv Covek')
